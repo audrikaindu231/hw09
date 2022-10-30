c5d9856e1a0b4c23d58851f80ed9ea3a3bedc941
audrikaindu231 <111930284+audrikaindu231@users.noreply.github.com>
2022-10-30 19:39:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='homework',
       url='https://github.com/audrikaindu231/hw09',
       author='Audrika Indu',
       author_email='cet6dn@virginia.edu'
-      license='MIT'
+      license='MIT',
       packages='Module09')
 
