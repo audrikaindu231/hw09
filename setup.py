87a6a28caf1c5c74dfe68c96c3e83bd183fc5bb5
audrikaindu231 <111930284+audrikaindu231@users.noreply.github.com>
2022-10-30 18:57:01 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setup.py import setup
+
+setup(name='homework',
+      version='0.1',
+      description='A Homework Package',
+      url='https://github.com/audrikaindu231/hw09',
+      author='Audrika Indu',
+      author_email='cet6dn@virginia.edu'
+      license='MIT'
+      packages='Module09'
+
